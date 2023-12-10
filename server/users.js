bfbc30b630a7fdabd63f37c7aca93a74f94851f3
paschalc24 <pcorrigan@umass.edu>
2023-12-10 00:00:40 -0500

    Added temporary local json db.

---
+++ server/users.js
@@ -0,0 +1,38 @@
+class Users {
+  constructor() {
+    // we use an in-memory "database"; this isn't persistent but is easy
+    this.users = {};
+  }
+
+  // Returns true iff the user exists.
+  findUser(username) {
+    if (!this.users[username]) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  // Returns true iff the password is the one we have stored (in plaintext = bad
+  // but easy).
+  validatePassword(name, pwd) {
+    if (!this.findUser(name)) {
+      return false;
+    }
+    if (this.users[name] !== pwd) {
+      return false;
+    }
+    return true;
+  }
+
+  // Add a user to the "database".
+  addUser(name, pwd) {
+    if (this.findUser(name)) {
+      return false;
+    }
+    this.users[name] = pwd;
+    return true;
+  }
+}
+
+export default new Users();
