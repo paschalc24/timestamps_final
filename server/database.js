f41fbae54c361b1299e437d9c0722f844f8e1364
paschalc24 <pcorrigan@umass.edu>
2023-12-14 18:27:49 -0500

    Added logging and add job to DB.

--- server/database.js
+++ server/database.js
@@ -9,6 +9,7 @@ class Database {
   }
 
   async connect() {
+    console.log(`${(process.pid)}: DB connecting`);
     this.client = new MongoClient(this.dburl, {
       serverApi: {
         version: ServerApiVersion.v1,
@@ -17,16 +18,20 @@ class Database {
       }
     });
     await this.client.connect();
+    console.log(`Connection successful`);
     this.db = this.client.db('auth');
     this.users = this.db.collection('timestamps_users');
+    this.jobs = this.db.collection('jobs');
   }
 
   async close() {
+    console.log(`${(process.pid)}: DB closing`);
     this.client.close()
   }
 
   // Returns true iff the user exists.
   async findUser(username) {
+    console.log(`${(process.pid)}: DB finding user`);
     const user = await this.users.findOne({ name: username });
     if (user === null) {
       console.log("USER NOT FOUND", user);
@@ -39,6 +44,7 @@ class Database {
   // Returns true iff the password is the one we have stored (in plaintext = bad
   // but easy).
   async validatePassword(name, pwd) {
+    console.log(`${(process.pid)}: DB validating pwd`);
     const userFound = await this.findUser(name);
     if (userFound === false) {
       return false;
@@ -51,6 +57,7 @@ class Database {
 
   // Add a user to the "database".
   async addUser(name, pwd) {
+    console.log(`${(process.pid)}: DB adding user`);
     const userFound = await this.findUser(name);
     if (userFound !== false) {
       return false;
@@ -59,6 +66,20 @@ class Database {
       await this.users.insertOne({name: name, pwd: pwd});
       return true;
     }
+  }
+
+  // Add a job to the "database".
+  async addJob(body, res) {
+    console.log(`${(process.pid)}: DB adding job`);
+    try {
+      await this.jobs.insertOne(body);
+      res.status(200).end();
+    }
+    catch(error) {
+      console.error(error);
+      res.status(500).end()
+    }
+    return true;
   }
 }
 
